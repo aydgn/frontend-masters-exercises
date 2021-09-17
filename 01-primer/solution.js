0aa51894d9698d4c525b510903517dbcc8a5edfb
Aydoğan <aydogan.gokberk@gmail.com>
2021-09-18 00:37:12 +0300

    todo comments deleted

--- 01-primer/solution.js
+++ 01-primer/solution.js
@@ -1,15 +1,13 @@
-// TODO: define addFavoriteBook(..) function
 function addFavoriteBook(bookname) {
   if (!bookname.includes("Great")) {
     favoriteBooks.push(bookname);
   }
 }
 
-// TODO: define printFavoriteBooks() function
 function printFavoriteBooks() {
   const totalBooks = favoriteBooks.length;
   console.log("Total number of books: " + totalBooks);
-  for (book of favoriteBooks) {
+  for (const book of favoriteBooks) {
     console.log(book);
   }
 }
@@ -22,5 +20,4 @@ addFavoriteBook("Crime & Punishment");
 addFavoriteBook("Great Expectations");
 addFavoriteBook("You Don't Know JS");
 
-// TODO: print out favorite books
 printFavoriteBooks();
